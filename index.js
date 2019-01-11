3afc31766382b38d9707046da415bbaeeb2ac537
Mingfang Duan <11203duan@gmail.com>
2019-01-11 16:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function logShout(string) {
   function logWhisper(string) {
     console.log('hello')
   }
+  
   function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) { 
     return "I can't hear you!"

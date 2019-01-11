0094439b15d737fc36364c3b03fc2ceabe1a60d9
Mingfang Duan <11203duan@gmail.com>
2019-01-11 16:47:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO') }
+  function logWhisper(string) {
+    console.log('hello')
+  }
+var lowercase = 
+  function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) { 
+    return "I can't hear you!"
+    }
+  }
+ function sayHiToGrandma(string) {
+   if(string.toUpperCase() === string) {
+     return "YES INDEED!"
+   }
+ }
+function sayHiToGrandma(string) {
+  if (string ==="I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
